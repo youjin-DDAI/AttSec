8b612b83c2a55e492d39d4165041564aff63135f
YOUJINKIM <51672387+youjin-DDAI@users.noreply.github.com>
2024-02-05 14:16:57 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -222,9 +222,117 @@ class T5_train_collate_fn(object):
 
         return batch_index, lengths, rep_pack, batch_labels, mask
 
+class T5_seg_att_collate_fn(object):
+    def __init__(self, dssp_dim):
+        self.tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_uniref50", do_lower_case=False )
+        self.model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50")
+        self.dssp_dim = dssp_dim
+
+    def __call__(self, batch):
+        seqs = [b[0] for b in batch]
+        labels = [b[1] for b in batch]
+        lengths = [b[2] for b in batch]
+
+        device = 'cuda'
+        self.model = self.model.to(device)
+        self.model = self.model.eval()
+
+        reps = []
+        atts = []
+        batch_seg_label = []
+        batch_index = []
+        batch_seg_index = []
+
+        cut = 500
+        for i, s in enumerate(seqs):
+            """if lengths[i]>cut:
+                s = s[:500]
+                lengths[i]=500
+                labels[i]=labels[i][:500]"""
+            s = ' '.join(s)
+            s = re.sub(r"[UZOB]", "X", s)
+            ids = self.tokenizer.batch_encode_plus([s])
+            input_ids = torch.tensor(ids['input_ids']).to(device)
+            attention_mask = torch.tensor(ids['attention_mask']).to(device)
+            with torch.no_grad():
+                embedding = self.model(input_ids=input_ids, attention_mask=attention_mask, output_attentions=True)
+            attention = embedding.attentions[-1].squeeze(0)
+            embedding = embedding.last_hidden_state.squeeze(0)
+            atts.append(attention)
+            reps.append(embedding[:-1, :])
+            batch_index.append(torch.tensor([i] * lengths[i]))
+            l = labels[i]
+            l_mask = make_gt_seg(l, self.dssp_dim).reshape(-1)
+            batch_seg_label.append(l_mask)
+            batch_seg_index.append(torch.tensor([i]*l_mask.size(0)))
+
+        batch_index = torch.cat(batch_index)
+        batch_seg_index = torch.cat(batch_seg_index)#None
+        batch_labels = torch.cat(labels)
+        batch_seg_label = torch.cat(batch_seg_label)
+        rep_pack = torch.nn.utils.rnn.pad_sequence(reps, batch_first=True, padding_value=0)
+        att_pack = attention_padding(atts, lengths)#None
+        seq_pad = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=21)
+        mask = create_padding_mask(seq_pad, 21)
+
+        return batch_index, batch_seg_index, lengths, rep_pack, att_pack, batch_labels, batch_seg_label, mask
 
 
+class T5_seg_att_train_collate_fn(object):
+    def __init__(self, dssp_dim):
+        self.tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_uniref50", do_lower_case=False )
+        self.model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50")
+        self.dssp_dim = dssp_dim
 
+    def __call__(self, batch):
+        seqs = [b[0] for b in batch]
+        labels = [b[1] for b in batch]
+        lengths = [b[2] for b in batch]
+
+        device = 'cuda'
+        self.model = self.model.to(device)
+        self.model = self.model.eval()
+
+        reps = []
+        atts = []
+        batch_seg_label = []
+        batch_index = []
+        batch_seg_index = []
+        cut = 500 
+        for i, s in enumerate(seqs):
+            if lengths[i]>cut:
+                start = random_start(lengths[i], cut)
+                s = s[start:start+cut]
+                lengths[i]=cut
+                labels[i]=labels[i][start:start+cut]
+            s = ' '.join(s)
+            s = re.sub(r"[UZOB]", "X", s)
+
+            ids = self.tokenizer.batch_encode_plus([s])
+            input_ids = torch.tensor(ids['input_ids']).to(device)
+            attention_mask = torch.tensor(ids['attention_mask']).to(device)
+            with torch.no_grad():
+                embedding = self.model(input_ids=input_ids, attention_mask=attention_mask, output_attentions=True)
+            attention = embedding.attentions[-1].squeeze(0)
+            embedding = embedding.last_hidden_state.squeeze(0)
+            atts.append(attention)
+            reps.append(embedding[:-1, :])
+            batch_index.append(torch.tensor([i] * lengths[i]))
+            l = labels[i]
+            l_mask = make_gt_seg(l, self.dssp_dim).reshape(-1)
+            batch_seg_label.append(l_mask)
+            batch_seg_index.append(torch.tensor([i]*l_mask.size(0)))
+
+        batch_index = torch.cat(batch_index)
+        batch_seg_index = torch.cat(batch_seg_index)#None
+        batch_labels = torch.cat(labels)
+        batch_seg_label = torch.cat(batch_seg_label)
+        rep_pack = torch.nn.utils.rnn.pad_sequence(reps, batch_first=True, padding_value=0)
+        att_pack = attention_padding(atts, lengths) #None
+        seq_pad = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=21)
+        mask = create_padding_mask(seq_pad, 21)
+
+        return batch_index, batch_seg_index, lengths, rep_pack, att_pack, batch_labels, batch_seg_label, mask
 
 
 
